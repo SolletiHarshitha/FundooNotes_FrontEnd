facad2993326d481f6529d7957680aa1fb1b1a82
Solleti Harshitha <solletiharshitha@gmail.com>
2021-09-16 00:59:03 +0530

    [Harshitha] Refactor: Login Page with validations

--- src/app/Components/login/login.component.ts
+++ src/app/Components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, Validators, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  LoginForm!: FormGroup;
+  hide = true;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.LoginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.pattern('^[a-z]{1,}[a-z0-9]*[-.+]{0,1}[a-z0-9]+@([a-z0-9]+)(\\.[a-z]{2,}){1,2}$')]),
+      password: new FormControl('', [Validators.required, Validators.pattern('^[A-Za-z0-9.!@$-_]{8,}'), Validators.minLength(8)])
+    });
   }
 
 }
