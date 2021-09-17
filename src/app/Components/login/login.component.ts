179cb2ddb210e617fc3514b0634af6ce005f514c
Solleti Harshitha <solletiharshitha@gmail.com>
2021-09-17 19:46:11 +0530

    [Harshitha]Add: Added login data to local storage

--- src/app/Components/login/login.component.ts
+++ src/app/Components/login/login.component.ts
@@ -3,6 +3,7 @@ import { FormControl, Validators, FormGroup } from '@angular/forms';
 import { UserServiceService } from 'src/app/Services/UserService/user-service.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { HttpErrorResponse } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -16,7 +17,7 @@ export class LoginComponent implements OnInit {
   constructor(
     private userService: UserServiceService,
     private snackBar: MatSnackBar,
-    
+    private router : Router
   ) { }
 
   ngOnInit(): void {
@@ -31,6 +32,16 @@ export class LoginComponent implements OnInit {
     {
       console.log(result);
       this.openSnackBar(result.message, '');
+      if(result.status == true){
+        const params =  {
+          UserId: result.data.userId,
+          FirstName: result.data.firstName,
+          LastName: result.data.lastName,
+          Email: result.data.email,
+          Token: result.resultMessage
+        }
+        localStorage.setItem('FundooUser', JSON.stringify(params));
+      }
     }, (error: HttpErrorResponse) => {
       if(!error.error.status){
         this.openSnackBar(error.error.message,'');
